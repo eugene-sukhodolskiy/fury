4b8bdd17f4c9ebf71c6b438e09c0976fb5c8febe
s_eugene_r <s_eugene_r@yahoo.com>
2020-01-11 04:55:50 +0200

    Added RoutesHelper module

--- TestApp/app.php
+++ TestApp/app.php
@@ -2,6 +2,7 @@
 
 use Fury\Modules\Router\Router;
 use Fury\Kernel\Events;
+use Fury\Modules\RoutesHelper\RoutesHelper;
 
 $router = new Router();
 
@@ -18,9 +19,11 @@ function events_handlers($events){
 }
 
 function routes_map($router){
-	$router -> get(['var1', 'var2'], '\TestApp\Welcome@hello');
-	$router -> uri('/testing', '\TestApp\Welcome@testing');
-	$router -> uri('/id/$id/$post', '\TestApp\Welcome@testing_with_params');
-	$router -> uri('/base-template', '\TestAPP\Welcome@template_test');
+	$rh = new RoutesHelper($router);
+	$rh -> uri() -> class('\TestApp\Welcome');
+	// $router -> get(['var1', 'var2'], '\TestApp\Welcome@hello');
+	// $router -> uri('/testing', '\TestApp\Welcome@testing');
+	// $router -> uri('/id/$id/$post', '\TestApp\Welcome@testing_with_params');
+	// $router -> uri('/base-template', '\TestAPP\Welcome@template_test');
 	return $router;
 }
