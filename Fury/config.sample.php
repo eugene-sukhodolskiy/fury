900b6664c7f4314035f397169f0fe1f25d31525a
s_eugene_r <s_eugene_r@yahoo.com>
2020-01-20 15:44:33 +0200

    clean

--- Fury/config.sample.php
+++ Fury/config.sample.php
@@ -1,15 +1,15 @@
 <?php
 
 return [
-	"app_name" => "test APP",
+	"app_name" => "AppName",
 	"debug" => true,
 	"db" => [
 		"dblib" => "mysql",
 		"host" => "127.0.0.1",
-		"dbname" => "fury",
+		"dbname" => "sample",
 		"charset" => "utf8",
 		"user" => "root",
-		"password" => ""
+		"password" => "pass"
 	],
 	"app_file" => "app.php",
 	"templates_folder" => "templates",
--- TestApp/templates/article.php
+++
@@ -1,5 +0,0 @@
-<? $this -> extends_from('base') ?>
-<article>
-	<h3>Article title</h3>
-	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum quas vero cumque qui praesentium inventore totam velit, illum temporibus ratione! Hic provident ad distinctio accusamus. Accusantium tempore, illum architecto, est magnam recusandae assumenda tempora sed. Quidem quo necessitatibus error a.</p>
-</article>
