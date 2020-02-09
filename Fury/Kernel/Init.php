9946c6d9a7a0c4137e350b95223b9ff7549006b2
s_eugene_r <s_eugene_r@yahoo.com>
2020-02-09 19:26:02 +0200

    Documantation of bootstrap class

--- Fury/Kernel/Init.php
+++ Fury/Kernel/Init.php
@@ -2,13 +2,40 @@
 
 namespace Fury\Kernel;
 
+/**
+ * Class: Init
+ * @author Eugene Sukhodolskiy <e.sukhodolskiy@outlook.com>
+ * @version 0.1
+ * Date: 09.02.2020
+ */
+
 class Init{
+	/**
+	 * Bootstrap class instance
+	 *
+	 * @var Bootstrap
+	 */
 	protected $bootstrap;
 
-	public function __construct($bootstrap){
+	/**
+	 * Constructor with params
+	 *
+	 * @method __construct
+	 *
+	 * @param  Bootstrap $bootstrap Bootstrap instance
+	 */
+	public function __construct(Bootstrap $bootstrap){
 		$this -> bootstrap = $bootstrap;
 	}
 
+	/**
+	 * Method for framework and application initialization
+	 * If you need changed order of initialization - ok, but be careful :)
+	 *
+	 * @method init
+	 *
+	 * @return void
+	 */
 	public function init(){
 		$this -> bootstrap -> init_config();
 		$this -> bootstrap -> init_consts();
